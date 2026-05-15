93fe8c47fb13d89e369edab83ec3b36d537cc1e5
mrae43 <khaelsm.mrae43@hmail.com>
2026-05-15 15:23:40 +0800

    feat(api): created a merge revision issue two migration branches (alembic/versions)

---
+++ apps/api/alembic/versions/446e7dda836c_merge_evidence_text_and_role_column_.py
@@ -0,0 +1,28 @@
+"""merge evidence_text and role column heads
+
+Revision ID: 446e7dda836c
+Revises: a1b2c3d4e5f6, a2b1c3d4e5f6
+Create Date: 2026-05-15 15:14:42.893198
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '446e7dda836c'
+down_revision: Union[str, Sequence[str], None] = ('a1b2c3d4e5f6', 'a2b1c3d4e5f6')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
