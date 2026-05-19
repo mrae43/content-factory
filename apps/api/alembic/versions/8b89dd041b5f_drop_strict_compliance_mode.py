3e59e9948b2232c5225f6f60337684418ac340af
mrae43 <khaelsm.mrae43@gmail.com>
2026-05-19 23:45:42 +0800

    fix(api): reformatted 1 file

--- apps/api/alembic/versions/8b89dd041b5f_drop_strict_compliance_mode.py
+++ apps/api/alembic/versions/8b89dd041b5f_drop_strict_compliance_mode.py
@@ -5,6 +5,7 @@ Revises: 0a560a3395a6
 Create Date: 2026-05-19 23:04:17.746942
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,21 +13,27 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '8b89dd041b5f'
-down_revision: Union[str, Sequence[str], None] = '0a560a3395a6'
+revision: str = "8b89dd041b5f"
+down_revision: Union[str, Sequence[str], None] = "0a560a3395a6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.drop_column('render_jobs', 'strict_compliance_mode', schema='factory')
+    op.drop_column("render_jobs", "strict_compliance_mode", schema="factory")
 
 
 def downgrade() -> None:
     """Downgrade schema."""
     op.add_column(
-        'render_jobs',
-        sa.Column('strict_compliance_mode', sa.BOOLEAN(), autoincrement=False, nullable=False, server_default=sa.text('true')),
-        schema='factory',
+        "render_jobs",
+        sa.Column(
+            "strict_compliance_mode",
+            sa.BOOLEAN(),
+            autoincrement=False,
+            nullable=False,
+            server_default=sa.text("true"),
+        ),
+        schema="factory",
     )
